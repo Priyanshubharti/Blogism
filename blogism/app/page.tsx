81881da6e78184bc2209fbcff339bab25924cfb6
Priyanshu Bharti <priyanshubharti41@gmail.com>
2024-01-02 20:49:20 +0530

    Adding Authentication

--- blogism/app/page.tsx
+++ blogism/app/page.tsx
@@ -1,7 +1,22 @@
+// import { getServerSession } from "next-auth"
+// import { authOptions } from "./api/auth/[...nextauth]/route"
+"use client";
 
+import { useSession } from "next-auth/react";
 
-export default function Home() {
-  return (
-<main> Hello World !</main>
-  )
+export default  function Home() {
+  
+    // const data = await getServerSession(authOptions)
+
+    const {data, status} = useSession();
+return <main>
+  <div>
+  Hello World !
+    </div> 
+    <div>
+      {JSON.stringify(data)}
+      {JSON.stringify(status)}
+    </div>
+    </main>
+  
 }
